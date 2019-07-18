80955cc5b9896a219ca9ab7d6124435310a4f5ae
Yaroslav <doctor355@yahoo.com>
2019-07-18 16:01:11 +0300

    fixing error

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
 
+import axios from 'axios';
+
 class App extends Component{
-  componentDidMount(){
-    console.log('React Running....');
+  constructor(props){
+    super(props);
+    this.state = {
+      paras: 4,
+      html: true,
+      text:''
+    }
   }
 
+  componentWillMount(){
+    this.getText();
+  }
+
+  getText(){
+    axios.get('https://loripsum.net/api/4/short/headers')
+      .then((response) => {
+        this.setState({text: response.data.text}, function(){
+          console.log(this.state);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
   render(){
     return (
       <div>
